5bcc33ea7e2fd2e93929894977f2c4658fa70961
Evan Morrison <evan@Evans-MacBook-Pro.local>
2018-05-07 17:22:30 -0500

    initial

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('BBQ Burger');
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
+
